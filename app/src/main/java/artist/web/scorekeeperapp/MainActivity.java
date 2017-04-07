71c5da184346b20f764fae32189c15b949e692b9
WEBARTIST\User <hennasingh04@gmail.com>
2017-04-07 05:40:14 +0100

    additions to code - alert dialog and random message on every hit

--- app/src/main/java/artist/web/scorekeeperapp/MainActivity.java
+++ app/src/main/java/artist/web/scorekeeperapp/MainActivity.java
@@ -1,8 +1,11 @@
 package artist.web.scorekeeperapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,33 +15,42 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    //static variables to serialize game data when the phone rotates
     static final String STARK_HEALTH_BAR = "starkHealth";
     static final String TARG_HEALTH_BAR = "targHealth";
-    static final String STARK_SCORE ="starkScore";
+    static final String STARK_SCORE = "starkScore";
     static final String TARG_SCORE = "targScore";
-    static final String MESSAGE_BOARD= "messageBoard";
-    static final String HITS_STARK ="hitScoreSt";
-    static final String HITS_TARG ="hitScoreTarg";
+    static final String MESSAGE_BOARD = "messageBoard";
+    static final String HITS_STARK = "hitScoreSt";
+    static final String HITS_TARG = "hitScoreTarg";
 
+    //variables to calculate progress bar value
     private ProgressBar healthStark, healthTargaryen;
 
-    private int[] attackMeasures={10,20,25,30};
-    private int randomValue, attackValue;
+    //random array to calculate attack intensity
+    private int[] attackMeasures = {10, 20, 25, 30};
 
+    //random array to send messages when user clicks on a hit
 
+    String []displayMessages = {"Good Hit","You Rock!!", "You are on Fire","UhhUuuuHuuuu"};
+
+    //Textviews to set and get scores, counters in the game
     private TextView scoreStark;
     private TextView scoreTargaryen;
     private TextView message;
     private TextView hitScoreStark;
     private TextView hitScoreTarg;
 
-    private int teamS=0;
-    private int teamT=0;
-    private int hitCounterSt=0;
-    private int hitCounterTarg=0;
+    //variables to calculate score, number of hits and progressbar values
+    private int teamS = 0;
+    private int teamT = 0;
+    private int hitCounterSt = 0;
+    private int hitCounterTarg = 0;
+    private int maxHealthStark = 100;
+    private int maxHealthTargaryen = 100;
 
-    private int maxHealthStark =100;
-    private int maxHealthTargaryen=100;
+    //variable to get new button reference
+    private Button newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,115 +59,193 @@ public class MainActivity extends AppCompatActivity {
         ShimmerFrameLayout layout = (ShimmerFrameLayout) findViewById(R.id.shimmerlayout);
         layout.startShimmerAnimation();
 
-        healthStark = (ProgressBar)findViewById(R.id.healthBarStark);
-        healthTargaryen=(ProgressBar)findViewById(R.id.healthBarTargaryen);
-        scoreStark = (TextView)findViewById(R.id.teamStark);
-        scoreTargaryen = (TextView)findViewById(R.id.teamTarg);
-        message = (TextView)findViewById(R.id.message);
-        hitScoreStark=(TextView)findViewById(R.id.hitStark);
-        hitScoreTarg=(TextView)findViewById(R.id.hitTarg);
+        healthStark = (ProgressBar) findViewById(R.id.healthBarStark);
+        healthTargaryen = (ProgressBar) findViewById(R.id.healthBarTargaryen);
+        scoreStark = (TextView) findViewById(R.id.teamStark);
+        scoreTargaryen = (TextView) findViewById(R.id.teamTarg);
+        message = (TextView) findViewById(R.id.message);
+        hitScoreStark = (TextView) findViewById(R.id.hitStark);
+        hitScoreTarg = (TextView) findViewById(R.id.hitTarg);
+        newGame = (Button)findViewById(R.id.reset);
 
-     }
+    }
 
-    public void attackOpponent(View view){
+    /** This method is called when user clicks on Attack Image Button
+     *
+     * @param
+     */
+    public void attackOpponent(View view) {
 
-        randomValue = (int)(Math.random()*4);
-        attackValue = attackMeasures[randomValue];
+       int  randomValue1 = (int) (Math.random() * 4);
+        int attackValue = attackMeasures[randomValue1];
+
+        int randomValue2 = (int)(Math.random()*4);
+        String attackMsg = displayMessages[randomValue2];
 
         Random randomGenerator = new Random();
-        if(randomGenerator.nextBoolean()) {
+        if (randomGenerator.nextBoolean()) {
 
             hitCounterSt++;
-            displayScoreTeamStark(attackValue);
+            displayScoreTeamStark(attackValue,attackMsg);
 
-        }
-        else{
+        } else {
             hitCounterTarg++;
-            displayScoreTeamTarg(attackValue);
+            displayScoreTeamTarg(attackValue,attackMsg);
         }
-     }
+    }
 
-       public void displayScoreTeamStark(int score){
-           maxHealthTargaryen = maxHealthTargaryen-score;
-           teamS= teamS + score;
-           if(maxHealthTargaryen<=0||teamS>=100){
-               gameOver();
-           }
-           healthTargaryen.setProgress(maxHealthTargaryen);
-           scoreStark.setText(String.valueOf(teamS));
-           hitScoreStark.setText(String.valueOf(hitCounterSt));
-       }
+    /** This method is internally called by the attackOpponent Method
+     * to display the team Stark score randomly selected by the attackOpponent
+     * Method
+     * @param score is an integer number randomly calculated to hit the opposite
+     *              team with that intensity
+     *  @param attackMsg is a string msg to display when a user hits
+     */
 
-       public void displayScoreTeamTarg(int score){
-           maxHealthStark=maxHealthStark-score;
-           teamT = teamT +score;
-           if(maxHealthStark<=0||teamT>=100){
-               gameOver();
-           }
-           healthStark.setProgress(maxHealthStark);
-           scoreTargaryen.setText(String.valueOf(teamT));
-           hitScoreTarg.setText(String.valueOf(hitCounterTarg));
-       }
-    public void gameOver(){
-        if(maxHealthStark<=0||teamT>=100){
+    public void displayScoreTeamStark(int score,String attackMsg) {
+        maxHealthTargaryen = maxHealthTargaryen - score;
+        teamS = teamS + score;
+        if (maxHealthTargaryen <= 0 || teamS >= 100) {
+            gameOver();
+        }
+        healthTargaryen.setProgress(maxHealthTargaryen);
+        scoreStark.setText(String.valueOf(teamS));
+        message.setText(attackMsg);
+        hitScoreStark.setText(String.valueOf(hitCounterSt));
+    }
+
+    /**
+     * This method is internally called by the attackOpponent Method
+     * to display the team Targayen score randomly selected by the attackOpponent
+     * Method
+     * @param score is an integer number randomly calculated to hit the opposite
+     *              team with that intensity
+     *  @param attackMsg is a string msg to display when a user hits
+     */
+
+    public void displayScoreTeamTarg(int score, String attackMsg) {
+        maxHealthStark = maxHealthStark - score;
+        teamT = teamT + score;
+        if (maxHealthStark <= 0 || teamT >= 100) {
+            gameOver();
+        }
+        healthStark.setProgress(maxHealthStark);
+        scoreTargaryen.setText(String.valueOf(teamT));
+        message.setText(attackMsg);
+        hitScoreTarg.setText(String.valueOf(hitCounterTarg));
+    }
+
+    /**
+     * This method is called when the health bar of either team goes zero or
+     * team score is greater than equal to 100
+     */
+
+    public void gameOver() {
+        if (maxHealthStark <= 0 || teamT >= 100) {
             healthStark.setProgress(maxHealthStark);
             scoreTargaryen.setText(String.valueOf(teamT));
-            message.setText("House Targaryen Won" +"\n" + "Game Over");
+            AlertDialog.Builder winAlert = new AlertDialog.Builder(this);
+            winAlert.setTitle("Game Over");
+            winAlert.setMessage("House Targaryen Won");
+            winAlert.setIcon(R.drawable.targaryen_team);
+            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+
+                    newMatch(newGame);
+                }
+            });
+
+            winAlert.create();
+            winAlert.show();
+            message.setText("House Targaryen Won!!");
 
 
-        }else {
+
+        } else {
             healthTargaryen.setProgress(maxHealthTargaryen);
             scoreStark.setText(String.valueOf(teamS));
-            message.setText("House Stark Won" + "\n" + "Game Over");
+            AlertDialog.Builder winAlert = new AlertDialog.Builder(this);
+            winAlert.setTitle("Game Over");
+            winAlert.setMessage("House Stark Won");
+            winAlert.setIcon(R.drawable.starks_team);
+            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+
+                    newMatch(newGame);
+                }
+            });
+
+            winAlert.create();
+            winAlert.show();
+
+            message.setText("House Stark Won!!");
+
         }
 
     }
 
-       public void newMatch(View view){
-           teamS=0;
-           teamT=0;
-           hitCounterTarg=0;
-           hitCounterSt=0;
-           scoreStark.setText(String.valueOf(teamS));
-           hitScoreStark.setText(String.valueOf(hitCounterSt));
-           healthStark.setProgress(100);
-           scoreTargaryen.setText(String.valueOf(teamT));
-           hitScoreTarg.setText(String.valueOf(hitCounterTarg));
-           healthTargaryen.setProgress(100);
-           message.setText("");
-       }
+    /**
+     * This method is called when the user clicks on New button
+     * for a new Match
+     * @param view is the button view passed when its clicked
+     */
 
-       public void onSaveInstanceState(Bundle outState){
-           super.onSaveInstanceState(outState);
-           outState.putInt(STARK_HEALTH_BAR,maxHealthStark );
-           outState.putInt(TARG_HEALTH_BAR,maxHealthTargaryen);
-           outState.putInt(STARK_SCORE,teamS );
-           outState.putInt(TARG_SCORE,teamT );
-           outState.putString(MESSAGE_BOARD,message.getText().toString());
-           outState.putInt(HITS_STARK,hitCounterSt);
-           outState.putInt(HITS_TARG,hitCounterTarg);
+    public void newMatch(View view) {
+        teamS = 0;
+        teamT = 0;
+        hitCounterTarg = 0;
+        hitCounterSt = 0;
+        scoreStark.setText(String.valueOf(teamS));
+        hitScoreStark.setText(String.valueOf(hitCounterSt));
+        healthStark.setProgress(100);
+        scoreTargaryen.setText(String.valueOf(teamT));
+        hitScoreTarg.setText(String.valueOf(hitCounterTarg));
+        healthTargaryen.setProgress(100);
+        message.setText("");
+    }
+
+    /**
+     * This is to save the state of the game when the phone
+     * is rotated to opposite mode
+     * @param outState is bundle variable to serialize all scores and game data
+     */
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STARK_HEALTH_BAR, maxHealthStark);
+        outState.putInt(TARG_HEALTH_BAR, maxHealthTargaryen);
+        outState.putInt(STARK_SCORE, teamS);
+        outState.putInt(TARG_SCORE, teamT);
+        outState.putString(MESSAGE_BOARD, message.getText().toString());
+        outState.putInt(HITS_STARK, hitCounterSt);
+        outState.putInt(HITS_TARG, hitCounterTarg);
 
 
-       }
+    }
 
-       public void onRestoreInstanceState(Bundle savedInstanceState){
+    /**
+     * This is called when activity is recreated after screen rotation
+     * @param savedInstanceState is a variable to deserialize saved game data
+     */
 
-           healthStark = (ProgressBar)findViewById(R.id.healthBarStark);
-           healthTargaryen=(ProgressBar)findViewById(R.id.healthBarTargaryen);
-           scoreStark = (TextView)findViewById(R.id.teamStark);
-           scoreTargaryen = (TextView)findViewById(R.id.teamTarg);
-           message = (TextView)findViewById(R.id.message);
-           hitScoreStark=(TextView)findViewById(R.id.hitStark);
-           hitScoreTarg=(TextView)findViewById(R.id.hitTarg);
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
 
-               scoreStark.setText(String.valueOf(savedInstanceState.getInt(STARK_SCORE)));
-               scoreTargaryen.setText(String.valueOf(savedInstanceState.getInt(TARG_SCORE)));
-               healthStark.setProgress(savedInstanceState.getInt(STARK_HEALTH_BAR));
-               healthTargaryen.setProgress(savedInstanceState.getInt(TARG_HEALTH_BAR));
-               message.setText(savedInstanceState.getString(MESSAGE_BOARD));
-               hitScoreStark.setText(String.valueOf(savedInstanceState.getInt(HITS_STARK)));
-               hitScoreTarg.setText(String.valueOf(savedInstanceState.getInt(HITS_TARG)));
+        healthStark = (ProgressBar) findViewById(R.id.healthBarStark);
+        healthTargaryen = (ProgressBar) findViewById(R.id.healthBarTargaryen);
+        scoreStark = (TextView) findViewById(R.id.teamStark);
+        scoreTargaryen = (TextView) findViewById(R.id.teamTarg);
+        message = (TextView) findViewById(R.id.message);
+        hitScoreStark = (TextView) findViewById(R.id.hitStark);
+        hitScoreTarg = (TextView) findViewById(R.id.hitTarg);
 
-       }
+        scoreStark.setText(String.valueOf(savedInstanceState.getInt(STARK_SCORE)));
+        scoreTargaryen.setText(String.valueOf(savedInstanceState.getInt(TARG_SCORE)));
+        healthStark.setProgress(savedInstanceState.getInt(STARK_HEALTH_BAR));
+        healthTargaryen.setProgress(savedInstanceState.getInt(TARG_HEALTH_BAR));
+        message.setText(savedInstanceState.getString(MESSAGE_BOARD));
+        hitScoreStark.setText(String.valueOf(savedInstanceState.getInt(HITS_STARK)));
+        hitScoreTarg.setText(String.valueOf(savedInstanceState.getInt(HITS_TARG)));
+
+    }
 
 }

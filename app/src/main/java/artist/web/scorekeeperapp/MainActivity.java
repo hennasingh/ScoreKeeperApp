cfee32c21f8915cd97403c87bd42e2751003a505
WEBARTIST\User <hennasingh04@gmail.com>
2017-05-13 08:07:28 +0100

    changes to string, xml and java file, reformatted code

--- app/src/main/java/artist/web/scorekeeperapp/MainActivity.java
+++ app/src/main/java/artist/web/scorekeeperapp/MainActivity.java
@@ -16,24 +16,20 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     //static variables to serialize game data when the phone rotates
-    static final String STARK_HEALTH_BAR = "starkHealth";
-    static final String TARG_HEALTH_BAR = "targHealth";
-    static final String STARK_SCORE = "starkScore";
-    static final String TARG_SCORE = "targScore";
-    static final String MESSAGE_BOARD = "messageBoard";
-    static final String HITS_STARK = "hitScoreSt";
-    static final String HITS_TARG = "hitScoreTarg";
-
+    private static final String STARK_HEALTH_BAR = "starkHealth";
+    private static final String TARG_HEALTH_BAR = "targHealth";
+    private static final String STARK_SCORE = "starkScore";
+    private static final String TARG_SCORE = "targScore";
+    private static final String MESSAGE_BOARD = "messageBoard";
+    private static final String HITS_STARK = "hitScoreSt";
+    private static final String HITS_TARG = "hitScoreTarg";
+    private String[] displayMessages = {"Good Hit", "You Rock!!", "You are on Fire", "UhhUuuuHuuuu"};
     //variables to calculate progress bar value
     private ProgressBar healthStark, healthTargaryen;
 
+    //random array to send messages when user clicks on a hit
     //random array to calculate attack intensity
     private int[] attackMeasures = {10, 20, 25, 30};
-
-    //random array to send messages when user clicks on a hit
-
-    String []displayMessages = {"Good Hit","You Rock!!", "You are on Fire","UhhUuuuHuuuu"};
-
     //Textviews to set and get scores, counters in the game
     private TextView scoreStark;
     private TextView scoreTargaryen;
@@ -66,43 +62,44 @@ public class MainActivity extends AppCompatActivity {
         message = (TextView) findViewById(R.id.message);
         hitScoreStark = (TextView) findViewById(R.id.hitStark);
         hitScoreTarg = (TextView) findViewById(R.id.hitTarg);
-        newGame = (Button)findViewById(R.id.reset);
-
+        newGame = (Button) findViewById(R.id.reset);
     }
 
-    /** This method is called when user clicks on Attack Image Button
+    /**
+     * This method is called when user clicks on Attack Image Button
      *
      * @param
      */
     public void attackOpponent(View view) {
 
-       int  randomValue1 = (int) (Math.random() * 4);
+        int randomValue1 = (int) (Math.random() * 4);
         int attackValue = attackMeasures[randomValue1];
 
-        int randomValue2 = (int)(Math.random()*4);
+        int randomValue2 = (int) (Math.random() * 4);
         String attackMsg = displayMessages[randomValue2];
 
         Random randomGenerator = new Random();
         if (randomGenerator.nextBoolean()) {
 
             hitCounterSt++;
-            displayScoreTeamStark(attackValue,attackMsg);
+            displayScoreTeamStark(attackValue, attackMsg);
 
         } else {
             hitCounterTarg++;
-            displayScoreTeamTarg(attackValue,attackMsg);
+            displayScoreTeamTarg(attackValue, attackMsg);
         }
     }
 
-    /** This method is internally called by the attackOpponent Method
+    /**
+     * This method is internally called by the attackOpponent Method
      * to display the team Stark score randomly selected by the attackOpponent
      * Method
-     * @param score is an integer number randomly calculated to hit the opposite
-     *              team with that intensity
-     *  @param attackMsg is a string msg to display when a user hits
+     *
+     * @param score     is an integer number randomly calculated to hit the opposite
+     *                  team with that intensity
+     * @param attackMsg is a string msg to display when a user hits
      */
-
-    public void displayScoreTeamStark(int score,String attackMsg) {
+    public void displayScoreTeamStark(int score, String attackMsg) {
         maxHealthTargaryen = maxHealthTargaryen - score;
         teamS = teamS + score;
         if (maxHealthTargaryen <= 0 || teamS >= 100) {
@@ -118,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is internally called by the attackOpponent Method
      * to display the team Targayen score randomly selected by the attackOpponent
      * Method
-     * @param score is an integer number randomly calculated to hit the opposite
-     *              team with that intensity
-     *  @param attackMsg is a string msg to display when a user hits
+     *
+     * @param score     is an integer number randomly calculated to hit the opposite
+     *                  team with that intensity
+     * @param attackMsg is a string msg to display when a user hits
      */
-
     public void displayScoreTeamTarg(int score, String attackMsg) {
         maxHealthStark = maxHealthStark - score;
         teamT = teamT + score;
@@ -139,7 +136,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the health bar of either team goes zero or
      * team score is greater than equal to 100
      */
-
     public void gameOver() {
         if (maxHealthStark <= 0 || teamT >= 100) {
             healthStark.setProgress(maxHealthStark);
@@ -148,19 +144,14 @@ public class MainActivity extends AppCompatActivity {
             winAlert.setTitle("Game Over");
             winAlert.setMessage("House Targaryen Won");
             winAlert.setIcon(R.drawable.targaryen_team);
-            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
-
+            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     newMatch(newGame);
                 }
             });
-
             winAlert.create();
             winAlert.show();
             message.setText("House Targaryen Won!!");
-
-
-
         } else {
             healthTargaryen.setProgress(maxHealthTargaryen);
             scoreStark.setText(String.valueOf(teamS));
@@ -168,28 +159,23 @@ public class MainActivity extends AppCompatActivity {
             winAlert.setTitle("Game Over");
             winAlert.setMessage("House Stark Won");
             winAlert.setIcon(R.drawable.starks_team);
-            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
-
+            winAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     newMatch(newGame);
                 }
             });
-
             winAlert.create();
             winAlert.show();
-
             message.setText("House Stark Won!!");
-
         }
-
     }
 
     /**
      * This method is called when the user clicks on New button
      * for a new Match
+     *
      * @param view is the button view passed when its clicked
      */
-
     public void newMatch(View view) {
         teamS = 0;
         teamT = 0;
@@ -207,6 +193,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This is to save the state of the game when the phone
      * is rotated to opposite mode
+     *
      * @param outState is bundle variable to serialize all scores and game data
      */
 
@@ -219,15 +206,13 @@ public class MainActivity extends AppCompatActivity {
         outState.putString(MESSAGE_BOARD, message.getText().toString());
         outState.putInt(HITS_STARK, hitCounterSt);
         outState.putInt(HITS_TARG, hitCounterTarg);
-
-
     }
 
     /**
      * This is called when activity is recreated after screen rotation
+     *
      * @param savedInstanceState is a variable to deserialize saved game data
      */
-
     public void onRestoreInstanceState(Bundle savedInstanceState) {
 
         healthStark = (ProgressBar) findViewById(R.id.healthBarStark);
@@ -245,7 +230,5 @@ public class MainActivity extends AppCompatActivity {
         message.setText(savedInstanceState.getString(MESSAGE_BOARD));
         hitScoreStark.setText(String.valueOf(savedInstanceState.getInt(HITS_STARK)));
         hitScoreTarg.setText(String.valueOf(savedInstanceState.getInt(HITS_TARG)));
-
     }
-
 }
